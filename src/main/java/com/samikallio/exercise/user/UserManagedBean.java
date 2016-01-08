8606b2410b2bdfd329b6c844b5ae7fce595609c5
Sami Kallio <phinaliumz@outlook.com>
2016-01-08 18:34:32 +0200

    Minor modifications

--- src/main/java/com/samikallio/exercise/user/UserManagedBean.java
+++ src/main/java/com/samikallio/exercise/user/UserManagedBean.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.samikallio.exercise.user;
 
 import javax.ejb.EJB;
@@ -18,13 +13,58 @@ import javax.enterprise.context.RequestScoped;
 @RequestScoped
 public class UserManagedBean {
     
+    /**
+     * Enterprise bean for database transactions
+     */
     @EJB
     private UserEnterpriseBeanLocal userEJB;
     
+    /**
+     * Entity, which is the data that will be persisted to database
+     */
     private UserEntity userEntity;
+    
+    /*
+     * Variables for webpage information 
+     */
+    private String firstName;
+    private String lastName;
+    private Boolean isFemale;
+    private String reasonForApplying;
 
     public UserManagedBean() {
         
     }
-    
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Boolean getIsFemale() {
+        return isFemale;
+    }
+
+    public void setIsFemale(Boolean isFemale) {
+        this.isFemale = isFemale;
+    }
+
+    public String getReasonForApplying() {
+        return reasonForApplying;
+    }
+
+    public void setReasonForApplying(String reasonForApplying) {
+        this.reasonForApplying = reasonForApplying;
+    }
 }
